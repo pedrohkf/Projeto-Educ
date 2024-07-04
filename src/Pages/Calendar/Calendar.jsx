56c3f16df2c6b6ad0f00794e52c53fad13e232d1
Pedro Henrique <phkfaria@gmail.com>
2024-07-04 14:05:44 -0300

    começando calendario

--- src/Pages/Calendar/Calendar.jsx
+++ src/Pages/Calendar/Calendar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Calendar.module.css";
 import Dots from "../../Assets/dots.svg?react";
 import ArrowLeft from "../../Assets/arrow-left.svg?react";
@@ -27,38 +27,81 @@ export const Calendar = () => {
               <ArrowRight />
             </div>
             <div className={styles.calendar}>
-              {/* <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div> */}
+              <div className={styles.hours}>
+                <div>1 pm</div>
+                <div>1 pm</div>
+                <div>1 pm</div>
+                <div>1 pm</div>
+                <div>1 pm</div>
+              </div>
+              <div className={styles.containerDays}>
+                <div className={styles.days}>
+                  <div>
+                    <p id={styles.number}>1</p>
+                    <p>Segunda</p>
+                  </div>
+                  <div>
+                    <p id={styles.number}>2</p>
+                    <p>Terça</p>
+                  </div>
+                  <div>
+                    <p id={styles.number}>3</p>
+                    <p>Quarta</p>
+                  </div>
+                  <div>
+                    <p id={styles.number}>4</p>
+                    <p>Quinta</p>
+                  </div>
+                  <div>
+                    <p id={styles.number}>5</p>
+                    <p>Sexta</p>
+                  </div>
+                  <div>
+                    <p id={styles.number}>6</p>
+                    <p>Sabado</p>
+                  </div>
+                  <div>
+                    <p id={styles.number}>7</p>
+                    <p>Domingo</p>
+                  </div>
+                </div>
+                <div className={styles.markers}>
+                  <div>
+                    <div>Tarefas</div>
+                  </div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
